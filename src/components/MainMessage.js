2ae2c521482e7a958636fe6271dddfc1db07bcf3
Max <max_karl@icloud.com>
2018-09-16 16:37:08 +0200

    add styling

--- src/components/MainMessage.js
+++ src/components/MainMessage.js
@@ -4,7 +4,7 @@ import TypeWriter from 'react-typewriter';
 class MainMessage extends Component {
   render() {
     return (
-      <div className="main_message">
+      <div className={this.props.isError ? "main_message error_message" : "main_message"}>
          <TypeWriter typing={1}>{this.props.message}</TypeWriter>
       </div>
     );
