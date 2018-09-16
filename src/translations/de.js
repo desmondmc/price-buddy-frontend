88ba1b58cbb89331004697bee26787399d87a98c
Desmond McNamee <desmondmcnamee@gmail.com>
2018-09-16 22:07:17 +0200

    Small translation change

--- src/translations/de.js
+++ src/translations/de.js
@@ -6,7 +6,7 @@ export default {
   try_again: 'Something went wrong, please try again',
   unknown_error: 'Hmm something is wrong',
   email_already_exists: 'Looks like that email already exists',
-  ask_valid_email: 'Please enter a valid email',
+  ask_valid_email: 'That\'s not a valid email...',
   password_too_short: 'Must be at least 6 charaters',
   product_link: 'https://www.amazon.com/product/B0ABC9JWK',
   placeholder_email: 'john@mail.com',
